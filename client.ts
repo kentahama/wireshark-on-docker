fc513b07063f41bd0c4243fec1557092e4b68acd
Kenta Hamanaka <kenta.hamanaka@gmail.com>
2023-01-27 16:56:43 +0900

    :sparkles: Add docker-wireshark

--- client.ts
+++ client.ts
@@ -1,10 +1,14 @@
 const URL = Deno.env.get("URL") ?? "ws://localhost:5000";
 
-const ws = new WebSocket(URL);
-ws.onmessage = (event) => console.log("get:", event.data);
-ws.onopen = () => {
-  setInterval(() => {
-    const data = "Hello";
-    ws.send(data);
-  }, 2_000);
-};
+function main() {
+  const ws = new WebSocket(URL);
+  ws.onmessage = (event) => console.log("get:", event.data);
+  ws.onopen = () => {
+    setInterval(() => {
+      const data = "Hello";
+      ws.send(data);
+    }, 2_000);
+  };
+}
+
+setTimeout(main, 5_000);
